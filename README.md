0e8494e2618a04809ec2060dae7dc1c6172a506c
monsterxcn <monsterxcn@gmail.com>
2022-11-25 23:20:07 +0800

    :tada: Hello World

---
+++ README.md
@@ -0,0 +1,55 @@
+<h1 align="center">NoneBot Plugin GsCode</h1></br>
+
+
+<p align="center">🤖 用于查询原神前瞻直播兑换码的 NoneBot2 插件</p></br>
+
+
+<p align="center">
+  <a href="https://github.com/monsterxcn/nonebot-plugin-gscode/actions">
+    <img src="https://img.shields.io/github/workflow/status/monsterxcn/nonebot-plugin-gscode/Build%20distributions?style=flat-square" alt="actions">
+  </a>
+  <a href="https://raw.githubusercontent.com/monsterxcn/nonebot-plugin-gscode/master/LICENSE">
+    <img src="https://img.shields.io/github/license/monsterxcn/nonebot-plugin-gscode?style=flat-square" alt="license">
+  </a>
+  <a href="https://pypi.python.org/pypi/nonebot-plugin-gscode">
+    <img src="https://img.shields.io/pypi/v/nonebot-plugin-gscode?style=flat-square" alt="pypi">
+  </a>
+  <img src="https://img.shields.io/badge/python-3.8+-blue?style=flat-square" alt="python"><br />
+</p></br>
+
+
+| ![image](https://user-images.githubusercontent.com/22407052/204017447-84f300f4-0df2-44df-ac3e-4bc72a47d816.png) | ![image](https://user-images.githubusercontent.com/22407052/204016397-2c2063cb-9e0d-4060-808d-32b2bb84bc69.png) |
+|:--:|:--:|
+
+
+## 安装方法
+
+
+如果你正在使用 2.0.0.beta1 以上版本 NoneBot，推荐使用以下命令安装：
+
+
+```bash
+# 从 nb_cli 安装
+python3 -m nb plugin install nonebot-plugin-gscode
+
+# 或从 PyPI 安装
+python3 -m pip install nonebot-plugin-gscode
+```
+
+
+## 使用说明
+
+
+插件响应 `gscode` / `兑换码` 开头的消息，返回一组包含兑换码信息的合并转发消息。
+
+
+经测试，兑换码接口返回与前瞻直播有 2 分钟左右延迟，应为正常现象，请耐心等待。
+
+
+插件依赖 [@Mrs4s/go-cqhttp](https://github.com/Mrs4s/go-cqhttp) 的合并转发接口，如需启用私聊响应请务必安装 [v1.0.0-rc2](https://github.com/Mrs4s/go-cqhttp/releases/tag/v1.0.0-rc2) 以上版本的 go-cqhttp。
+
+
+## 特别鸣谢
+
+
+[@nonebot/nonebot2](https://github.com/nonebot/nonebot2/) | [@Mrs4s/go-cqhttp](https://github.com/Mrs4s/go-cqhttp) | [@Le-niao/Yunzai-Bot](https://github.com/Le-niao/Yunzai-Bot)
---
+++ nonebot_plugin_gscode/data_source.py
@@ -0,0 +1,122 @@
+from re import findall
+from time import time, strftime, localtime
+from traceback import format_exc
+from typing import Dict, List
+
+from httpx import AsyncClient
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.log import logger
+
+
+async def getData(type, data: Dict = {}) -> Dict:
+    """米哈游接口请求"""
+
+    url = {
+        "index": f"https://api-takumi.mihoyo.com/event/bbslive/index?act_id={data.get('actId', '')}",
+        "mi18n": f"https://webstatic.mihoyo.com/admin/mi18n/bbs_cn/{data.get('mi18n', '')}/{data.get('mi18n', '')}-zh-cn.json",
+        "code": f"https://webstatic.mihoyo.com/bbslive/code/{data.get('actId', '')}.json?version=1&time={int(time())}",
+        "actId": "https://bbs-api.mihoyo.com/post/wapi/getPostFullInCollection?collection_id=1280130&gids=2&order_type=2",
+    }
+    async with AsyncClient() as client:
+        try:
+            res = await client.get(url[type])
+            return res.json()
+        except Exception as e:
+            logger.error(f"{type} 接口错误 {e.__class__.__name__}\n{format_exc()}")
+            return {"error": f"{type} 接口错误 {e.__class__.__name__}"}
+
+
+async def getActId() -> str:
+    """获取 ``act_id``"""
+
+    ret = await getData("actId")
+    if ret.get("error") or ret.get("retcode") != 0:
+        return ""
+    posts = sorted(
+        ret.get("data", {}).get("posts", []),
+        key=lambda p: p["post"]["created_at"],
+        reverse=True,
+    )
+    return posts[0]["post"]["content"].replace("[链接]", "") if posts else ""
+
+
+async def getCodes() -> List[MessageSegment]:
+    """生成最新前瞻直播兑换码合并转发消息"""
+
+    actId = await getActId()
+    if not actId:
+        return [
+            MessageSegment.node_custom(
+                user_id=2854196310,
+                nickname="原神前瞻直播",
+                content=Message(MessageSegment.text("没有找到 actId！")),
+            )
+        ]
+
+    indexRes = await getData("index", {"actId": actId})
+    mi18n = indexRes.get("data", {}).get("mi18n", "")
+    if not mi18n:
+        return [
+            MessageSegment.node_custom(
+                user_id=2854196310,
+                nickname="原神前瞻直播",
+                content=Message(MessageSegment.text("没有找到 mi18n！")),
+            )
+        ]
+
+    mi18nRes = await getData("mi18n", {"mi18n": mi18n})
+    codeRes = await getData("code", {"actId": actId})
+    nickname = mi18nRes.get("act-title", "").replace("特别节目", "") or "原神前瞻直播"
+    if indexRes.get("data", {}).get("remain", 0) or not len(codeRes):
+        return [
+            MessageSegment.node_custom(
+                user_id=2854196310,
+                nickname=nickname,
+                content=Message(MessageSegment.image(mi18nRes["pc-kv"])),
+            ),
+            MessageSegment.node_custom(
+                user_id=2854196310,
+                nickname=nickname,
+                content=Message(
+                    MessageSegment.text(
+                        f"预计第一个兑换码发放时间为 "
+                        f"{strftime('%H:%M:%S', localtime(int(mi18nRes['time1'])))}"
+                        "\n\n* 官方接口数据有 2 分钟左右延迟，请耐心等待下~"
+                    )
+                ),
+            ),
+        ]
+
+    nextCode = (
+        ""
+        if len(codeRes) == 3
+        else (
+            "，下一个兑换码发放时间为 "
+            + strftime("%H:%M:%S", localtime(int(mi18nRes[f"time{len(codeRes) + 1}"])))
+        )
+    )
+    codes = [
+        MessageSegment.node_custom(
+            user_id=2854196310,
+            nickname=nickname,
+            content=Message(
+                MessageSegment.text(
+                    f"当前发放了 {len(codeRes)} 个兑换码{nextCode}\n{mi18nRes['exchange-tips']}"
+                )
+            ),
+        )
+    ]
+    for codeInfo in codeRes:
+        gifts = findall(
+            r">\s*([\u4e00-\u9fa5]+|\*[0-9]+)\s*<",
+            codeInfo["title"].replace("&nbsp;", " "),
+        )
+        codes.append(
+            MessageSegment.node_custom(
+                user_id=2854196310,
+                nickname="+".join(g for g in gifts if not g[-1].isdigit()) or nickname,
+                content=Message(MessageSegment.text(codeInfo["code"])),
+            )
+        )
+
+    return codes
