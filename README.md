74c968b30da0bc6c97e357dc0818e87938e8c4ab
monsterxcn <monsterxcn@gmail.com>
2023-01-04 20:54:19 +0800

    :zap: 使用原神米游姬帖子获取 `act_id`

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 <p align="center">
   <a href="https://github.com/monsterxcn/nonebot-plugin-gscode/actions">
-    <img src="https://img.shields.io/github/workflow/status/monsterxcn/nonebot-plugin-gscode/Build%20distributions?style=flat-square" alt="actions">
+    <img src="https://img.shields.io/github/actions/workflow/status/monsterxcn/nonebot-plugin-gscode/publish.yml?branch=main&style=flat-square" alt="actions">
   </a>
   <a href="https://raw.githubusercontent.com/monsterxcn/nonebot-plugin-gscode/master/LICENSE">
     <img src="https://img.shields.io/github/license/monsterxcn/nonebot-plugin-gscode?style=flat-square" alt="license">
--- nonebot_plugin_gscode/data_source.py
+++ nonebot_plugin_gscode/data_source.py
@@ -1,6 +1,6 @@
+import json
 from re import findall
 from time import time, strftime, localtime
-from traceback import format_exc
 from typing import Dict, List
 
 from httpx import AsyncClient
@@ -15,15 +15,15 @@ async def getData(type, data: Dict = {}) -> Dict:
         "index": f"https://api-takumi.mihoyo.com/event/bbslive/index?act_id={data.get('actId', '')}",
         "mi18n": f"https://webstatic.mihoyo.com/admin/mi18n/bbs_cn/{data.get('mi18n', '')}/{data.get('mi18n', '')}-zh-cn.json",
         "code": f"https://webstatic.mihoyo.com/bbslive/code/{data.get('actId', '')}.json?version=1&time={int(time())}",
-        "actId": "https://bbs-api.mihoyo.com/post/wapi/getPostFullInCollection?collection_id=1280130&gids=2&order_type=2",
+        "actId": "https://bbs-api.mihoyo.com/painter/api/user_instant/list?offset=0&size=20&uid=75276550",  # 原神米游姬
     }
     async with AsyncClient() as client:
         try:
             res = await client.get(url[type])
             return res.json()
         except Exception as e:
-            logger.error(f"{type} 接口错误 {e.__class__.__name__}\n{format_exc()}")
-            return {"error": f"{type} 接口错误 {e.__class__.__name__}"}
+            logger.opt(exception=e).error(f"{type} 接口请求错误")
+            return {"error": f"[{e.__class__.__name__}] {type} 接口请求错误"}
 
 
 async def getActId() -> str:
@@ -32,12 +32,26 @@ async def getActId() -> str:
     ret = await getData("actId")
     if ret.get("error") or ret.get("retcode") != 0:
         return ""
-    posts = sorted(
-        ret.get("data", {}).get("posts", []),
-        key=lambda p: p["post"]["created_at"],
-        reverse=True,
-    )
-    return posts[0]["post"]["content"].replace("[链接]", "") if posts else ""
+
+    actId = ""
+    keywords = ["来看《原神》", "版本前瞻特别节目"]
+    for p in ret["data"]["list"]:
+        post = p.get("post", {}).get("post", {})
+        if not post:
+            continue
+        if not all(word in post["subject"] for word in keywords):
+            continue
+        shit = json.loads(post["structured_content"])
+        for segment in shit:
+            link = segment.get("attributes", {}).get("link", "")
+            if "观看直播" in segment.get("insert", "") and link:
+                matched = findall(r"act_id=(\d{8}ys\d{4})", link)
+                if matched:
+                    actId = matched[0]
+        if actId:
+            break
+
+    return actId
 
 
 async def getCodes() -> List[MessageSegment]:
@@ -49,7 +63,7 @@ async def getCodes() -> List[MessageSegment]:
             MessageSegment.node_custom(
                 user_id=2854196320,
                 nickname="原神前瞻直播",
-                content=Message(MessageSegment.text("没有找到 actId！")),
+                content=Message(MessageSegment.text("暂无前瞻直播资讯！")),
             )
         ]
 
@@ -82,7 +96,7 @@ async def getCodes() -> List[MessageSegment]:
                 content=Message(
                     MessageSegment.text(
                         f"预计第一个兑换码发放时间为 "
-                        f"{strftime('%H:%M:%S', localtime(int(mi18nRes['time1'])))}"
+                        f"{strftime('%m 月 %d 日 %H:%M', localtime(int(mi18nRes['time1'])))}"
                         "\n\n* 官方接口数据有 2 分钟左右延迟，请耐心等待下~"
                     )
                 ),
