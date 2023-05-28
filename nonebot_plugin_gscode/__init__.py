83c6cb69426444073e64947508e372c0c24f8db7
forchannot <100580891+forchannot@users.noreply.github.com>
2023-05-28 18:03:59 +0800

    :sparkles: 增加星穹铁道前瞻兑换码 (#8)
    * :sparkles: 增加星穹铁道前瞻兑换码
    
    * :rotating_light: [pre-commit] 自动修复
    
    * :bug: fix单行过长
    
    * :speech_balloon: 更新__plugin_meta__
    
    * :pencil2: 更改命名:小驼峰→蛇形
    
    * :art: 拆分为两个matcher
    
    * :rotating_light: [pre-commit] 自动修复
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nonebot_plugin_gscode/__init__.py
+++ nonebot_plugin_gscode/__init__.py
@@ -1,27 +1,34 @@
 from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import Bot
+from nonebot.internal.matcher import Matcher
 from nonebot.plugin import PluginMetadata, on_command
 from nonebot.adapters.onebot.v11.event import MessageEvent, GroupMessageEvent
 
-from .data_source import getMsg
+from .data_source import get_msg
 
 __plugin_meta__ = PluginMetadata(
     name="GsCode",
-    description="查询原神前瞻直播兑换码",
-    usage="""查询原神前瞻直播兑换码
+    description="查询原神/星穹铁道前瞻直播兑换码",
+    usage="""查询原神/星穹铁道前瞻直播兑换码
 注意：经测试，兑换码接口返回与前瞻直播有 2 分钟左右延迟，应为正常现象，请耐心等待。
 /gscode
-/兑换码""",
+/srcode""",
 )
 
-codeMatcher = on_command("gscode", aliases={"兑换码"}, priority=5)
+gs_code_matcher = on_command("gscode", aliases={"原神兑换码"}, priority=5)
+sr_code_matcher = on_command("srcode", aliases={"铁道兑换码", "星穹铁道兑换码"}, priority=5)
 
 
-@codeMatcher.handle()
-async def _(bot: Bot, event: MessageEvent, state: T_State):
+@gs_code_matcher.handle()
+@sr_code_matcher.handle()
+async def _(bot: Bot, event: MessageEvent, state: T_State, matcher: Matcher):
     if str(state["_prefix"]["command_arg"]):
-        await codeMatcher.finish()
-    codes = await getMsg()
+        await matcher.finish()
+    codes = (
+        await get_msg("gs")
+        if isinstance(matcher, gs_code_matcher)
+        else await get_msg("sr")
+    )
     if isinstance(event, GroupMessageEvent):
         await bot.send_group_forward_msg(group_id=event.group_id, messages=codes)
     else:
